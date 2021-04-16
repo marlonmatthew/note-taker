9530c16d93e04d2e43e2a1d9dff03a58372e0677
Marlon Matthew <mbm.drums@gmail.com>
2021-04-16 16:34:43 -0700

    apiRoutes.js added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,27 @@
 const router = require("express").Router();
 const noteTaker = require("../db/noteTaker");
+
+router.get("/notes", function (req, res) {
+  noteTaker
+    .get_note()
+    .then(function (notes) {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+router.post("/notes", function (req, res) {
+  noteTaker
+    .add(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
+router.delete("/notes/:id", function (req, res) {
+  noteTaker
+    .remove(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
