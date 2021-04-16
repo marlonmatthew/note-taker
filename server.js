d58743aef7ba1580971f93b12bfcf40bfcad1441
Marlon Matthew <mbm.drums@gmail.com>
2021-04-16 15:12:23 -0700

    Added server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
